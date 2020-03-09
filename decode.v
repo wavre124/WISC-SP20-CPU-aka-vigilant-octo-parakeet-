f2a0331d4a0ae597d6265fe74820f95acb0056a5
Kshitij Wavre <kshitij@rockhopper-08.cs.wisc.edu>
2020-03-08 21:55:31 -0500

    Finished up decode stage and fixed a few things

--- decode.v
+++ decode.v
@@ -1,11 +1,57 @@
 /*
    CS/ECE 552 Spring '20
-  
+
    Filename        : decode.v
    Description     : This is the module for the overall decode stage of the processor.
 */
-module decode (/* TODO: Add appropriate inputs/outputs for your decode stage here*/);
+module decode (clk, rst, Data_one, Data_two, err, inst, ALU_op, branch_jump_op, PC_src, Dst_reg, Ext_op
+               Ext_sign, Reg_write, Jump, Branch, Mem_read, Mem_write, JAL, Mem_reg
+               Mem_en, Excp, ALU_src, InvR1, InvR2, Sign, Cin);
 
    // TODO: Your code here
-   
+
+   parameter N = 16;
+
+   input clk, rst;
+   input [N-1:0] inst;
+   input [N-1:0] PC;
+   input [N-1:0] wb_data;
+
+   output [N-1:0] Data_one; // Rs
+   output [N-1:0] Data_two; // Rt
+   output err;
+
+   output [3:0] ALU_op;
+   output [2:0] branch_jump_op;
+   output [1:0] PC_src, Dst_reg;
+   output [1:0] Ext_op;
+   output Ext_sign, Reg_write, Jump, Branch, Mem_read, Mem_write, JAL, Mem_reg, Mem_en;
+   output Excp, ALU_src;
+   output InvR1, InvR2, Sign, Cin;
+
+   output [N-1:0] br_ju_addr;
+
+   wire [N-1:0] write_data;
+   wire [N-1:0] bj_write_data;
+   wire [2:0] write_sel;
+   wire [N-1:0] immediate;
+
+   localparam R7 = 3'b111;
+
+   assign write_data = (JAL) ? bj_write_data : wb_data;
+
+   mux4_1 write_sel_mux[3:0](.InA(inst[4:2]), .InB(inst[7:5]), .InC(inst[10:8]), .InD(R7), .S(Dst_reg), .Out(write_sel));
+
+   control ctrl_blk(.inst(inst), .ALU_op(ALU_op), .branch_jump_op(branch_jump_op), .PC_src(PC_src), .Dst_reg(Dst_reg), .Ext_op(Ext_op)
+                  .Ext_sign(Ext_sign), .Reg_write(Reg_write), .Jump(Jump), .Branch(Branch), .Mem_read(Mem_read), .Mem_write(Mem_write), .JAL(JAL), .Mem_reg(Mem_reg)
+                  .Mem_en(Mem_en), .Excp(Excp), .ALU_src(ALU_src), .InvR1(InvR1), .InvR2(InvR2), .Sign(Sign), .Cin(Cin));
+
+   regFile_bypass regfile(.read1Data(Data_one), .read2Data(Data_two), .err(err), .clk(clk), .rst(rst),
+                          .read1RegSel(inst[10:8]), .read2RegSel(inst[7:5]), .writeRegSel(write_sel), .writeData(write_data), .writeEn(Reg_write));
+
+   extend ext_blk(.inst(inst), .ext_sign(Ext_sign), .ext_op(Ext_op), .ext_imm(immediate));
+
+   branch_jump bj_blk(.rs(Data_one), .PC(PC), .imm(immediate), .displacement(immediate), .branch(Branch), .jump(Jump),
+                      .branch_jump_op(branch_jump_op), .b_j_PC(br_ju_addr), .reg_data(bj_write_data));
+
 endmodule
--- fetch.v
+++ fetch.v
@@ -1,6 +1,6 @@
 /*
    CS/ECE 552 Spring '20
-  
+
    Filename        : fetch.v
    Description     : This is the module for the overall fetch stage of the processor.
 */
@@ -9,45 +9,26 @@ module fetch (clk, EPC_reg ,rst, b_j_pc, curr_pc, PC_src, Mem_en, excp, instruct
   input wire [1:0] PC_src;
   input wire Mem_en, clk, rst, excp;
   input wire [15:0] b_j_pc, curr_pc, EPC_reg; //pcs being fed in from the branch address, jump address, and current pc for holds and normal instructions
-  wire [15:0] exception_pc 
-  output wire [15:0] incremented_pc;
+  wire [15:0] exception_pc
+  output [15:0] incremented_pc;
 
   wire [15:0] pc; //will be fed into our instruction memory
-  output wire [15:0]instruction; instruction received from instruction memory
-  
-  
+  output [15:0] instruction; //instruction received from instruction memory
+
+
   cla_16b adder (.A(curr_pc), .B(16'b0000_0000_0000_0010), .C_in(1'b0), .S(incremented_pc), .C_out());
 
-  
+
   //16 bit 2-1 mux for choosing 2 for exception handler or EPC after we return from the instruction
   mux2_1_N pc_mux1(.InA(EPC_reg), .InB(16'b0000_0000_0000_0010), .S(excp), .Out(exception_pc));
-  
-  
+
+
   //00 is for current pc i.e. HALT
   //01 is for incremented PC for a normal non jumping non branching instruction
   //10 is for branch_address
   //11 is for exception or EPC
   mux4_1_16b pc_mux2(.InA(curr_pc), .InB(incremented_pc), .InC(b_j_pc), .InD(exception_pc), .S(PC_src), .Out(pc));
- 
-
 
   memory2c instruction_memory(.data_out(instruction), data_in(16'b0000_0000_0000_0000), addr(pc), enable(Mem_en), wr(1'b0), createdump(1'b0), .clk(clk), .rst(rst));
- 
 
- 
- 
- 
- 
- 
- 
- 
-  
-   
-
-  
-  
-  
-
-   
-   
 endmodule

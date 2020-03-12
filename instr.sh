a9e164b403df4daecf6422883696c468da4a1e4a
Kshitij Wavre <kshitij@rockhopper-08.cs.wisc.edu>
2020-03-12 12:25:18 -0500

    Adding scripts for testing

---
+++ instr.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/sinclair/courses/cs552/spring2020/handouts/testprograms/public/complex_demo1
+export PATH=$PATH:/u/s/i/sinclair/public/html/courses/cs552/spring2020/handouts/bins
+wsrun.pl -list all.list proc_hier_bench *.v

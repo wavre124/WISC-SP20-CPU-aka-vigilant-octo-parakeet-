cd1e969712f1da669527bea350a7d1a68b5231f2
Kshitij Wavre <kshitij@royal-20.cs.wisc.edu>
2020-03-13 15:23:00 -0500

    PHASE 1: 100% working updated script

--- instr.sh
+++ instr.sh
@@ -1,3 +1,13 @@
-export PATH=$PATH:/sinclair/courses/cs552/spring2020/handouts/testprograms/public/complex_demo1
 export PATH=$PATH:/u/s/i/sinclair/public/html/courses/cs552/spring2020/handouts/bins
-wsrun.pl -list one.list proc_hier_bench *.v
+wsrun.pl -list all_simple.list proc_hier_bench *.v
+mv summary.log simple.summary.log
+wsrun.pl -list all_complex.list proc_hier_bench *.v
+mv summary.log complex.summary.log
+wsrun.pl -list all_ran_simple.list proc_hier_bench *.v
+mv summary.log rand_simple.summary.log
+wsrun.pl -list all_ran_complex.list proc_hier_bench *.v
+mv summary.log rand_complex.summary.log
+wsrun.pl -list all_ran_ctrl.list proc_hier_bench *.v
+mv summary.log rand_ctrl.summary.log
+wsrun.pl -list all_ran_mem.list proc_hier_bench *.v
+mv summary.log rand_mem.summary.log

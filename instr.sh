1b976a1b20e4a8b636d1ee1bc95d49e101461268
Kshitij Wavre <kshitij@royal-20.cs.wisc.edu>
2020-03-16 16:53:39 -0500

    finally got script working gdi

--- instr.sh
+++ instr.sh
@@ -11,3 +11,7 @@ wsrun.pl -list all_ran_ctrl.list proc_hier_bench *.v
 mv summary.log rand_ctrl.summary.log
 wsrun.pl -list all_ran_mem.list proc_hier_bench *.v
 mv summary.log rand_mem.summary.log
+echo "File names with FAILURES listed below: \n"
+grep -l "FAILED" *.log
+echo "FAILED tests listed below: \n"
+grep -i "FAILED" *.log

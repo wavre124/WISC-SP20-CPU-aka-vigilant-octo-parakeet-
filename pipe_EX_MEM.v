eef230e524d38d0019312552cb53a0dd76546952
dwaltzCS <43973979+dwaltzCS@users.noreply.github.com>
2020-03-26 23:32:24 -0500

    does not have full functionality
    we need to modify ex block to basically contain all control unit signals to pass along data

--- pipe_EX_MEM.v
+++ pipe_EX_MEM.v
@@ -4,6 +4,7 @@ module pipe_EX_MEM(clk, rst, data_out, data_out_o);
   input [15:0] data_out;
   output [15:0] data_out_o;
   dff data_out_flop[15:0](.q(data_out_o), .d(data_out), .clk(clk), .rst(rst));
+  //need to modify the ex block to contain basically all of the control unit signals so this pipe register block is not complete
 
 
 

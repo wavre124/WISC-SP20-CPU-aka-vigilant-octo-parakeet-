780256cb64e1b9bc1784fbc86f5668ab871ed9ee
dwaltzCS <43973979+dwaltzCS@users.noreply.github.com>
2020-03-29 22:10:02 -0500

    changed module name, removed JAL and inc pc

--- pipe_ID_EX.v
+++ pipe_ID_EX.v
@@ -1,4 +1,4 @@
-module pipe_IF_ID(clk, rst, ALU_op, branch_jump_op, EXT_op, Dst_reg, PC_src, ALU_src, EXT_sign, Reg_write, Mem_read, Mem_write, JAL, Mem_reg, Mem_en, Excp, Data_one, Data_two, br_ju_addr, inc_pc, opcode, rd, rs, err, ALU_op_o, branch_jump_op_o, Ext_op_o, Dst_reg_o, PC_src_o, ALU_src_o, Ext_sign_o, Reg_write_o, Mem_read_o, Mem_write_o, JAL_o, Mem_reg_o, Mem_en_o, Excp_o, Data_one_o, Data_two_o, br_ju_addr_o, inc_pc_o, opcode_o, rd_o, rs_o, err_o);
+module pipe_ID_EX(clk, rst, ALU_op, branch_jump_op, EXT_op, Dst_reg, PC_src, ALU_src, EXT_sign, Reg_write, Mem_read, Mem_write, Mem_reg, Mem_en, Excp, Data_one, Data_two, br_ju_addr, inc_pc, opcode, rd, rs, err, ALU_op_o, branch_jump_op_o, Ext_op_o, Dst_reg_o, PC_src_o, ALU_src_o, Ext_sign_o, Reg_write_o, Mem_read_o, Mem_write_o, JAL_o, Mem_reg_o, Mem_en_o, Excp_o, Data_one_o, Data_two_o, br_ju_addr_o, inc_pc_o, opcode_o, rd_o, rs_o, err_o);
  
  
  input clk, rst;
@@ -7,14 +7,13 @@ module pipe_IF_ID(clk, rst, ALU_op, branch_jump_op, EXT_op, Dst_reg, PC_src, ALU
   input [3:0] ALU_op;
   input [2:0] branch_jump_op; 
   input [1:0] Ext_op, Dst_reg, PC_src;
-  input ALU_src, Ext_sign, Reg_write, Mem_read, Mem_write, JAL, Mem_reg, Mem_en, Excp;
+  input ALU_src, Ext_sign, Reg_write, Mem_read, Mem_write, Mem_reg, Mem_en, Excp;
   /////////////////////////////////////////////////////////////////////////////////////////
   
   //inputs that are NOT CONTROL UNIT SIGNALS/////////////////////////////////////////////////
    input [15:0] Data_one; // Rs data
    input [15:0] Data_two; // Rt data
    input [15:0] br_ju_addr;
-   input [15:0] inc_pc;
    input [4:0] opcode; 
    input [2:0] rd;//register number
    input [2:0] rs;//register number
@@ -25,14 +24,13 @@ module pipe_IF_ID(clk, rst, ALU_op, branch_jump_op, EXT_op, Dst_reg, PC_src, ALU
   output [3:0] ALU_op_o;
   output [2:0] branch_jump_op_o; 
   output [1:0] Ext_op_o, Dst_reg_o, PC_src_o;
-  output ALU_src_o, Ext_sign_o, Reg_write_o, Mem_read_o, Mem_write_o, JAL_o, Mem_reg_o, Mem_en_o, Excp_o;
+  output ALU_src_o, Ext_sign_o, Reg_write_o, Mem_read_o, Mem_write_o, Mem_reg_o, Mem_en_o, Excp_o;
   ///////////////////////////////////////////////////////////////////////////////////
   
   //outputs that are NOT CONTROL UNIT SIGNALS///////////////////////////////////////////////
    output [15:0] Data_one_o; // Rs data
    output [15:0] Data_two_o; // Rt data
    output [15:0] br_ju_addr_o;
-   output [15:0] inc_pc_o; 
    output [4:0] opcode_o;
    output [2:0] rd_o;
    output [2:0} rs_o;
@@ -51,7 +49,6 @@ module pipe_IF_ID(clk, rst, ALU_op, branch_jump_op, EXT_op, Dst_reg, PC_src, ALU
     dff Reg_write_flop(.q(Reg_write_o), .d(Reg_write), .clk(clk), .rst(rst));
     dff Mem_read_flop(.q(Mem_read_o), .d(Mem_read), .clk(clk), .rst(rst));
     dff Mem_write_flop(.q(Mem_write_o), .d(Mem_write), .clk(clk), .rst(rst));
-    dff JAL_flop(.q(JAL_o), .d(JAL), .clk(clk), .rst(rst));
     dff Mem_reg_flop(.q(Mem_reg_o), .d(Mem_reg), .clk(clk), .rst(rst));
     dff Mem_en_flop(.q(Mem_en_o), .d(Mem_en), .clk(clk), .rst(rst));
     dff Excp_flop(.q(Excp_o), .d(Excp), .clk(clk), .rst(rst));
@@ -63,7 +60,6 @@ module pipe_IF_ID(clk, rst, ALU_op, branch_jump_op, EXT_op, Dst_reg, PC_src, ALU
     dff Data_one_flop[15:0](.q(Data_one_o), .d(Data_one), .clk(clk), .rst(rst));
     dff Data_two_flop[15:0](.q(Data_two_o), .d(Data_two), .clk(clk), .rst(rst));
     dff br_ju_addr_flop[15:0](.q(br_ju_addr_o), .d(br_ju_addr), .clk(clk), .rst(rst));
-    dff inc_pc_flop[15:0](.q(inc_pc_o), .d(inc_pc), .clk(clk), .rst(rst));
     dff opcode_flop [4:0](.q(opcode_o), .d(opcode), .clk(clk), .rst(rst));
     dff rd_flop [2:0] (.q(rd_0), .d(rd), .clk(clk), .rst(rst));
     dff rs_flop [2:0] (.q(rs_0), .d(rs), .clk(clk), .rst(rst));

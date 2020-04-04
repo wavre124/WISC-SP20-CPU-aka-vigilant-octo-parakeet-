a6595a2aed925534f18b90d18680001ce8364994
dwaltzCS <43973979+dwaltzCS@users.noreply.github.com>
2020-04-03 22:29:27 -0500

    Add files via upload

--- hazard_det.v
+++ hazard_det.v
@@ -33,8 +33,8 @@ localparam jal = 5'b00110;
 localparam jalr = 5'b00111;
 localparam jr = 5'b00101;
 localparam lbi = 5'b11000;
-localparam store = 5'b10000; 
-localparam slbi = 5'b10010; 
+localparam store = 5'b10000;
+localparam slbi = 5'b10010;
 localparam R7 = 3'b111;
 wire [4:0] EX_MEM_op;
 wire [4:0] MEM_wb_op;
@@ -42,57 +42,82 @@ wire [4:0] MEM_wb_op;
 assign EX_MEM_op = EX_MEM_ins[15:11];
 assign MEM_wb_op = MEM_wb_ins[15:11];
 
-wire ex_mem_reg_valid_rd;
-assign ex_mem_reg_valid_rd = EX_MEM_reg_write & EX_MEM_valid_rd;
-wire rs_rt_1;
-assign rs_rt_1 = (rd_ID_EX == rt  | rd_ID_EX == rs);
-wire mem_reg_valid_rd;
-assign mem_reg_valid_rd = MEM_wb_reg_write & EX_MEM_valid_rd;
-wire rs_rt_2;
-assign rs_rt_2 = (rd_EX_MEM == rt) | (rd_EX_MEM == rs);
-wire rs_rt_d;
-assign rs_rt_d = (rd_ID_EX == RD);
-wire rs_rt_d2;
-assign rs_rt_d2 = (rd_EX_MEM == RD) | (rd_EX_MEM == RD);
-wire write_rs_ex_mem;
-assign write_rs_ex_mem = ((EX_MEM_op == lbi) | (EX_MEM_op == stu) | (EX_MEM_op == slbi));
-wire write_rs_mem_wb;
-assign write_rs_mem_wb = ((MEM_wb_op == lbi) | (MEM_wb_op == stu) | (MEM_wb_op == slbi));
-wire rs_reg_ex_mem;
-assign rs_reg_ex_mem =  (rs_ID_EX == RD);
+wire valD_regW_1; //used to check if the instruction is writing to RD ex-mem pipe
+assign valD_regW_1 = EX_MEM_reg_write & EX_MEM_valid_rd;
 
-wire rs_reg_mem_wb;
-assign rs_reg_mem_wb = (rs_EX_MEM == RD);
+wire equal_rs_rt; //used to check if there is a data dependency with an instruction writing to RD
+assign equal_rs_rt = (rd_ID_EX == rt  | rd_ID_EX == rs);
 
-wire jal_jalr1;
-assign jal_jalr1 = ((EX_MEM_op == jal) | (EX_MEM_op == jalr));
-wire jal_jalr2; 
-assign jal_jalr2 = ((MEM_wb_op == jal) | (MEM_wb_op == jalr)); 
+wire valD_regW_2; //used to check if instruction is writing to RD from mem-wb pipe
+assign valD_regW_2 = MEM_wb_reg_write & MEM_wb_valid_rd;
 
-wire st_stu;
+wire equal_rs_rt2; //used to check if there is a data dependency with an instruction from mem-wb pipe
+assign equal_rs_rt2 = (rd_EX_MEM == rt) | (rd_EX_MEM == rs);
+
+wire rs_rt_r7;
+assign rs_rt_r7 = (rt == R7) | (rs == R7);
+
+wire equals_RD_1; //used to check if there is a data dependency for store and stu from ex-mem pipe
+assign equals_RD_1 = (rd_ID_EX == RD);
+
+wire equals_RD_2; //used to check if there is a data dependency for store and stu from mem-wb pipe
+assign equals_RD_2 = (rd_EX_MEM == RD);
+
+wire write_Rs_1; //lbi, stu, and slbi all write to RS so we need to check for those data dependencies from ex-mem pipe
+assign write_Rs_1 = ((EX_MEM_op == lbi) | (EX_MEM_op == stu) | (EX_MEM_op == slbi));
+
+wire write_Rs_2;//lbi, stu, and slbi all write to RS so we need to check for those data dependencies from mem-wb pipe
+assign write_Rs_2 = ((MEM_wb_op == lbi) | (MEM_wb_op == stu) | (MEM_wb_op == slbi));
+
+wire rs_equal_rd_1; //stu and store need RD so checking if lbi, slbi, or stu Rs = RD for stu and store from ex-mem pipe
+assign rs_equal_rd_1 =  (rs_ID_EX == RD);
+
+wire rs_equal_rs_rt; //checking if rs for lbi/slbi/stu is writing to same Rs
+assign rs_equal_rs_rt = (rs_ID_EX == rt) | (rs_ID_EX == rs);
+
+wire rs_equal_rs_rt2;
+assign rs_equal_rs_rt2 = (rs_EX_MEM == rt) | (rs_EX_MEM == rs);
+
+wire rs_equal_rd_2; //stu and store need RD so checking if lbi, slbi, or stu Rs = RD for stu and store mem-wb pipe
+assign rs_equal_rd_2 = (rs_EX_MEM == RD);
+
+wire r7_write; //jal and jalr both write to r7 so need to check if those instructions are in front of us
+assign r7_write = ((EX_MEM_op == jal) | (EX_MEM_op == jalr));
+
+wire r7_write_2; //jal and jalr both write to r7 so check if those instructions are in front of us
+assign r7_write_2 = ((MEM_wb_op == jal) | (MEM_wb_op == jalr));
+
+wire st_stu; //these instructions need RD in decode so waiting for that
 assign st_stu = (opcode == store) | (opcode == stu);
-wire jalr_jr;
+
+wire jalr_jr; //these instructions need RS in decode so checking if one of those
 assign jalr_jr = (opcode == jalr) | (opcode == jr);
 
-wire lbi_stall;
+wire lbi_stall; //never stall on lbi so checking for that
 assign lbi_stall = (opcode == lbi);
 
-assign stall_decode = (ex_mem_reg_valid_rd & rs_rt_1 & (~lbi_stall)) ? 1'b1 :
-                      
-                      (mem_reg_valid_rd & rs_rt_2 & (~lbi_stall)) ? 1'b1 :
-                      
-                      ((jalr_jr) & ((ex_mem_reg_valid_rd & (rd_ID_EX == rs)) | (mem_reg_valid_rd & (rd_EX_MEM == rs)) | (write_rs_ex_mem & (rs_ID_EX == rs)) |
-                      (write_rs_mem_wb & (rs_EX_MEM == rs)) | (jal_jalr1 & (rs == R7)) | (jal_jalr2 & (rs == R7)))) ? 1'b1 :  
-			                
-                      ((st_stu) & (~lbi_stall) & (((ex_mem_reg_valid_rd & rs_rt_d) | (mem_reg_valid_rd & rs_rt_d2)) | (write_rs_ex_mem & rs_reg_ex_mem) | (write_rs_mem_wb &                                rs_reg_mem_wb))) ? 1'b1 :
-                      
-                      ((jal_jalr1) & (~lbi_stall) & ((st_stu & (RD == R7)) | (rs_rt_1))) ? 1'b1 :
-                      
-                      ((jal_jalr2) & (~lbi_stall) &((st_stu & (RD == R7)) | (rs_rt_2))) ? 1'b1 :
-                      
-                      ((write_rs_ex_mem) & (~lbi_stall) & ((rs_ID_EX == rt) | (rs_ID_EX == rs))) ? 1'b1 :
-	                  	
-                      ((write_rs_mem_wb) & (~lbi_stall) & ((rs_EX_MEM == rt) | (rs_EX_MEM == rs))) ? 1'b1 : 1'b0;
+assign stall_decode = (((valD_regW_1 & equal_rs_rt) | (r7_write &  rs_rt_r7) | (write_Rs_1 & rs_equal_rs_rt)) & (~lbi_stall)) ? 1'b1 :
+                      //checking first pipe register to see if it is writing to RD and that RD equals Rs/Rt
+                      //and it is not lbi because lbi never stalls and also checking if instruction in front us is writing
+                      //to R7 and that R7 is equal to Rs/Rt, also checking if isntruction in front us is writing to rs and that rs equals rs/rt ..... maybe include valid Rt signal???
+
+                      (((valD_regW_2 & equal_rs_rt2) | (r7_write_2 &  rs_rt_r7) | (write_Rs_2 & rs_equal_rs_rt2)) & (~lbi_stall)) ? 1'b1 ://same logic as comment above
+
+                      ((jalr_jr) & ((valD_regW_1 & (rd_ID_EX == rs)) | (valD_regW_2 & (rd_EX_MEM == rs)) | (write_Rs_1 & (rs_ID_EX == rs)) |
+                      (write_Rs_2 & (rs_EX_MEM == rs)) | (r7_write & (rs == R7)) | (r7_write_2 & (rs == R7)))) ? 1'b1 :
+			                //checking to see if it is jal/jalr which need RS, and then checking if instructions in front us in pipeline are writing to an RD that equals RS, also checking
+                      //then checking if instructions in front of us write to RS and that RS equals Rs for jal/jalr. or checking if instruction in front of us is writing to R7 and
+                      //R7 equals RS
+
+                      ((st_stu) & (~lbi_stall) & (((valD_regW_1 & equals_RD_1) | (valD_regW_2 & equals_RD_2)) | (write_Rs_1 & rs_equal_rd_1) | (write_Rs_2 &
+                        rs_equal_rd_2) | (r7_write & (RD == R7)) | (r7_write_2 & (RD == R7) ))) ? 1'b1 :
+                      //checking if it is instruction that needs Rd in decode state and then checking if instruction in front of us writes to Rd and that Rd is equal to Rd in decode
+                      //stage and checking if the instruciton in front of us is writing to Rs and that Rs equals Rd then checking to see if instruction in front of us is writing to                       //R7 and RD = R7
+
+
+
+
+                      ((write_Rs_2) & (~lbi_stall) & ((rs_EX_MEM == rt) | (rs_EX_MEM == rs))) ? 1'b1 : 1'b0;
 
 assign flush_fetch = (PC_source == 2'b10) ? 1'b1 : 1'b0;
 

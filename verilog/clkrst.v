913d2dc613269c8b7ea9b51e59cd732fe2d6cf41
Kshitij Wavre <kshitij@royal-05.cs.wisc.edu>
2020-04-24 11:41:52 -0500

    whoops added 0 to clkrst.v by overwriting it with clkrst from cache

--- verilog/clkrst.v
+++ verilog/clkrst.v
@@ -37,7 +37,7 @@ module clkrst (clk, rst, err);
     end
     always @(posedge clk) begin
     	cycle_count = cycle_count + 1;
-	if (cycle_count > 1000000) begin
+	if (cycle_count > 100000) begin
 		$display("hmm....more than 100000 cycles of simulation...error?\n");
 		$finish;
 	end

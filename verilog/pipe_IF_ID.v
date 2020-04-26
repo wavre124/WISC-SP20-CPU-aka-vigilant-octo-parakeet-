c7781c03d30f984245ceb7db2c43abd76c2618a9
Kshitij Wavre <kshitij@royal-30.cs.wisc.edu>
2020-04-26 11:30:49 -0500

    wtf

--- verilog/pipe_IF_ID.v
+++ verilog/pipe_IF_ID.v
@@ -1,5 +1,5 @@
 module pipe_IF_ID(clk, rst, instruction, incremented_pc, flush_fetch, stall_decode, ID_instruction, ID_incremented_pc, halt, EX_instruction,
-                  inst_mis_align, ID_inst_mis_align, stall_ins_mem);
+                  inst_mis_align, ID_inst_mis_align);
 
 input clk, rst;
 input [15:0] instruction, EX_instruction; //instruction received from instruction memory
@@ -8,22 +8,20 @@ input flush_fetch;
 input stall_decode;
 input halt;
 input inst_mis_align;
-input stall_ins_mem;
+
 output [15:0] ID_instruction;
 output [15:0] ID_incremented_pc;
 output ID_inst_mis_align;
-wire stall_helper;
 
 wire [15:0] mux_instruction, mux2_instruction;
 wire [15:0] mux_pc;
 wire [4:0] opcode;
 wire nop_stall;
-assign stall_helper = stall_ins_mem | nop_stall;
 assign opcode = EX_instruction[15:11];
 assign nop_stall = flush_fetch | halt;
 localparam rti = 5'b00011;
 wire nop_stall_help;
-assign nop_stall_help = (opcode == rti) ? 1'b0 : stall_helper;
+assign nop_stall_help = (opcode == rti) ? 1'b0 : nop_stall;
 localparam nop  = 16'b0000_1000_0000_0000;
 
 mux2_1_N ins_mux(.InA(instruction), .InB(nop), .S(nop_stall_help), .Out(mux_instruction));

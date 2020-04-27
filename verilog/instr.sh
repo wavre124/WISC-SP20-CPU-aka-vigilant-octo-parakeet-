7ca8cf9b8826a468d6725649fddd2035eee47f07
Kshitij Wavre <kshitij@snares-06.cs.wisc.edu>
2020-04-27 15:58:27 -0500

    adding all demo3 tests

--- verilog/instr.sh
+++ verilog/instr.sh
@@ -2,24 +2,24 @@ SPATH=/u/k/s/kshitij/private/cs552/project/phase3/vigilant-octo-parakeet/scripts
 LPATH=/u/k/s/kshitij/private/cs552/project/phase3/vigilant-octo-parakeet/paths
 RPATH=/u/k/s/kshitij/private/cs552/project/phase3/vigilant-octo-parakeet/results/new_results
 
-#SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_simple.list proc_hier_pbench *.v
-#mv summary.log $RPATH/instTests.summary.log
+$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_simple.list proc_hier_pbench *.v
+mv summary.log $RPATH/instTests.summary.log
 $SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_complex.list proc_hier_pbench *.v
-#mv summary.log $RPATH/complex.summary.log
-#$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_simple.list proc_hier_pbench *.v
-#mv summary.log $RPATH/rand_simple.summary.log
-#$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_complex.list proc_hier_pbench *.v
-#mv summary.log $RPATH/rand_complex.summary.log
-#$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_ctrl.list proc_hier_pbench *.v
-#mv summary.log $RPATH/rand_ctrl.summary.log
-#$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_mem.list proc_hier_pbench *.v
-#mv summary.log $RPATH/rand_mem.summary.log
-#$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_demo_two.list proc_hier_pbench *.v
-#mv summary.log $RPATH/complex_demo2.summary.log
-#$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/unaligned.list proc_hier_pbench *.v
-#mv summary.log $RPATH/unaligned.summary.log
-$SPATH/wsrun.pl -align -pipe -list $LPATH/one.list proc_hier_pbench *.v
-#echo "File names with FAILURES listed below: "
-#grep -l "FAILED" $RPATH/*.log
-#echo "FAILED tests listed below: "
-#grep -i "FAILED" $RPATH/*.log
+mv summary.log $RPATH/complex.summary.log
+$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_simple.list proc_hier_pbench *.v
+mv summary.log $RPATH/rand_simple.summary.log
+$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_complex.list proc_hier_pbench *.v
+mv summary.log $RPATH/rand_complex.summary.log
+$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_ctrl.list proc_hier_pbench *.v
+mv summary.log $RPATH/rand_ctrl.summary.log
+$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_ran_mem.list proc_hier_pbench *.v
+mv summary.log $RPATH/rand_mem.summary.log
+$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/all_demo_two.list proc_hier_pbench *.v
+mv summary.log $RPATH/complex_demo2.summary.log
+$SPATH/wsrun.pl -align -brief -pipe -list $LPATH/unaligned.list proc_hier_pbench *.v
+mv summary.log $RPATH/unaligned.summary.log
+#$SPATH/wsrun.pl -align -pipe -list $LPATH/one.list proc_hier_pbench *.v
+echo "File names with FAILURES listed below: "
+grep -l "FAILED" $RPATH/*.log
+echo "FAILED tests listed below: "
+grep -i "FAILED" $RPATH/*.log
